7ac731e78d5aa2d52dc6dd29d33d81b5941901f2
Artem Arkhangelskiy <105560004+EternoDejaVu@users.noreply.github.com>
2022-11-13 17:22:51 +0300

    Update FilmsManager.java

--- src/main/java/FilmsManager.java
+++ src/main/java/FilmsManager.java
@@ -8,12 +8,12 @@ public class FilmsManager {
 
     public void add(String film) {
         String[] tmp = new String[films.length + 1];
-        int i;
-        for (i = 0; i < films.length; i++) ;
-        tmp[i] = films[i];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length -1] = film;
         films = tmp;
     }
-
     public String[] findAll() {
         return films;
     }
