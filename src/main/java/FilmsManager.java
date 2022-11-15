00a479900d3710e350ae3957ade9f3c8e4423f1b
Artem Arkhangelskiy <105560004+EternoDejaVu@users.noreply.github.com>
2022-11-15 19:58:04 +0300

    Update FilmsManager.java

--- src/main/java/FilmsManager.java
+++ src/main/java/FilmsManager.java
@@ -1,7 +1,11 @@
 public class FilmsManager {
     protected String[] films = new String[0];
     protected int limit;
-    public FilmsManager() { this.limit = 10; }
+
+    public FilmsManager() {
+        this.limit = 10;
+    }
+
     public FilmsManager(int limit) {
         this.limit = limit;
     }
@@ -11,9 +15,10 @@ public class FilmsManager {
         for (int i = 0; i < films.length; i++) {
             tmp[i] = films[i];
         }
-        tmp[tmp.length -1] = film;
+        tmp[tmp.length - 1] = film;
         films = tmp;
     }
+
     public String[] findAll() {
         return films;
     }
