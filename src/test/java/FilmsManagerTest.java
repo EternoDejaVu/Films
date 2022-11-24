1b38aefb298f650e20fb03a8c8e611fbe460b695
Artem Arkhangelskiy <105560004+EternoDejaVu@users.noreply.github.com>
2022-11-24 17:56:16 +0300

    Update FilmsManagerTest.java

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -21,29 +21,6 @@ public class FilmsManagerTest {
     }
 
     @Test
-    public void ShouldNotAddFilmsOverTheLimit() {
-        FilmsManager manager = new FilmsManager(10);
-
-        manager.add("film1");
-        manager.add("film2");
-        manager.add("film3");
-        manager.add("film4");
-        manager.add("film5");
-        manager.add("film6");
-        manager.add("film7");
-        manager.add("film8");
-        manager.add("film9");
-        manager.add("film10");
-        manager.add("film11");
-        manager.add("film12");
-
-        String[] actual = manager.findAll();
-        String[] expected = {"film1", "film2", "film3", "film4", "film5", "film6", "film7", "film8", "film9", "film10", "film11"};
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
     public void ShouldAddFilmsEqualTheLimit() {
         FilmsManager manager = new FilmsManager(10);
 
@@ -100,4 +77,3 @@ public class FilmsManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
-}
